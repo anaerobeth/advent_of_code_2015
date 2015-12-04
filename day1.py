d05e3ef3b8492717d439e0040bc40a0999d7c07f
Beth Tenorio <betsky98@gmail.com>
2015-12-04 18:05:28 -0500

    solution to day 1 - gold

--- day1.py
+++ day1.py
@@ -6,3 +6,17 @@ with open('day1_input.txt', 'r') as f:
 
 print(up - down)
 
+with open('day1_input.txt', 'r') as f:
+    ''' Santa enters the basement when the current_floor is -1'''
+    current_floor = 0
+    steps = f.readlines()[0]
+    for index, value in enumerate(steps):
+        if value == '(':
+            current_floor += 1
+        else:
+            current_floor -= 1
+
+        if current_floor == -1:
+            print index + 1
+            break
+
