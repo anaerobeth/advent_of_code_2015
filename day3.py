c66bcf7fe643cc504b270c4dfab248e221ec0dd0
Beth Tenorio <betsky98@gmail.com>
2015-12-04 20:36:33 -0500

    dry up code

--- day3.py
+++ day3.py
@@ -12,9 +12,11 @@ def translate_symbol_to_move(value):
     return moves.get(value)
 
 
-def get_cell(x, y):
-    return "{0},{1}".format(x, y)
-
+def locate_house(x, y, move_x, move_y):
+    x += move_x
+    y += move_y
+    cell = "{0},{1}".format(x, y)
+    return x, y, cell
 
 def houses_with_presents(santas = 1):
     with open('day3_input.txt', 'r') as f:
@@ -25,20 +27,14 @@ def houses_with_presents(santas = 1):
             move_x, move_y = translate_symbol_to_move(value)
             if santas == 2:
                 if index % 2 == 0:
-                    santa_x += move_x
-                    santa_y += move_y
-                    cell = get_cell(santa_x, santa_y)
+                    santa_x, santa_y, location = locate_house(santa_x, santa_y, move_x, move_y)
                 else:
-                    robo_x += move_x
-                    robo_y += move_y
-                    cell = get_cell(robo_x, robo_y)
+                    robo_x, robo_y, location = locate_house(robo_x, robo_y, move_x, move_y)
             elif santas == 1:
-                santa_x += move_x
-                santa_y += move_y
-                cell = get_cell(santa_x, santa_y)
+                santa_x, santa_y, location = locate_house(santa_x, santa_y, move_x, move_y)
             else:
                 raise Exception('Maximum of two santas, please')
-            house_locations.append(cell)
+            house_locations.append(location)
     return len(set(house_locations))
 
 print houses_with_presents()
