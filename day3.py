c4fcfff98a529f28dd62579187651fbc70c7dc8f
Beth Tenorio <betsky98@gmail.com>
2015-12-04 19:34:20 -0500

    solution to day 3 - gold

--- day3.py
+++ day3.py
@@ -16,13 +16,20 @@ house_locations = ['0,0']
 
 with open('day3_input.txt', 'r') as f:
     steps = f.readlines()[0].rstrip()
-    x = 0
-    y = 0
-    for value in steps:
+    santa_x = 0
+    santa_y = 0
+    robo_x = 0
+    robo_y = 0
+    for index, value in enumerate(steps):
         move_x, move_y = translate_symbol_to_move(value)
-        x = x + move_x
-        y = y + move_y
-        cell = "{0},{1}".format(x,y)
+        if index % 2 == 0:
+            santa_x = santa_x + move_x
+            santa_y = santa_y + move_y
+            cell = "{0},{1}".format(santa_x,santa_y)
+        else:
+            robo_x = robo_x + move_x
+            robo_y = robo_y + move_y
+            cell = "{0},{1}".format(robo_x,robo_y)
         house_locations.append(cell)
 
 print house_locations
