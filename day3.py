3b8689987b12c88c943b90f6491ac86a9ebb0f80
Beth Tenorio <betsky98@gmail.com>
2015-12-04 20:09:29 -0500

    raise exception if an invalid number of santas is passed

--- day3.py
+++ day3.py
@@ -12,6 +12,10 @@ def translate_symbol_to_move(value):
     return moves.get(value)
 
 
+def get_cell(x, y):
+    return "{0},{1}".format(x, y)
+
+
 def houses_with_presents(santas = 1):
     with open('day3_input.txt', 'r') as f:
         steps = f.readlines()[0].rstrip()
@@ -23,18 +27,20 @@ def houses_with_presents(santas = 1):
                 if index % 2 == 0:
                     santa_x += move_x
                     santa_y += move_y
-                    cell = "{0},{1}".format(santa_x,santa_y)
+                    cell = get_cell(santa_x, santa_y)
                 else:
                     robo_x += move_x
                     robo_y += move_y
-                    cell = "{0},{1}".format(robo_x,robo_y)
-                house_locations.append(cell)
-            else:
+                    cell = get_cell(robo_x, robo_y)
+            elif santas == 1:
                 santa_x += move_x
                 santa_y += move_y
-                cell = "{0},{1}".format(santa_x,santa_y)
+                cell = get_cell(santa_x, santa_y)
+            else:
+                raise Exception('Maximum of two santas, please')
             house_locations.append(cell)
     return len(set(house_locations))
 
 print houses_with_presents()
 print houses_with_presents(santas = 2)
+print houses_with_presents(santas = 3)
