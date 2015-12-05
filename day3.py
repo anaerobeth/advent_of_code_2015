49626fd6b58a9ac4c855548a198d1761f411b2b8
Beth Tenorio <betsky98@gmail.com>
2015-12-04 19:50:50 -0500

    refactor method to work for both 1 or 2 santas

--- day3.py
+++ day3.py
@@ -12,26 +12,29 @@ def translate_symbol_to_move(value):
     return moves.get(value)
 
 
-house_locations = ['0,0']
+def houses_with_presents(santas = 1):
+    with open('day3_input.txt', 'r') as f:
+        steps = f.readlines()[0].rstrip()
+        santa_x, santa_y, robo_x, robo_y = 0, 0, 0, 0
+        house_locations = ['0,0']
+        for index, value in enumerate(steps):
+            move_x, move_y = translate_symbol_to_move(value)
+            if santas == 2:
+                if index % 2 == 0:
+                    santa_x += move_x
+                    santa_y += move_y
+                    cell = "{0},{1}".format(santa_x,santa_y)
+                else:
+                    robo_x += move_x
+                    robo_y += move_y
+                    cell = "{0},{1}".format(robo_x,robo_y)
+                house_locations.append(cell)
+            else:
+                santa_x += move_x
+                santa_y += move_y
+                cell = "{0},{1}".format(santa_x,santa_y)
+            house_locations.append(cell)
+    return len(set(house_locations))
 
-with open('day3_input.txt', 'r') as f:
-    steps = f.readlines()[0].rstrip()
-    santa_x = 0
-    santa_y = 0
-    robo_x = 0
-    robo_y = 0
-    for index, value in enumerate(steps):
-        move_x, move_y = translate_symbol_to_move(value)
-        if index % 2 == 0:
-            santa_x = santa_x + move_x
-            santa_y = santa_y + move_y
-            cell = "{0},{1}".format(santa_x,santa_y)
-        else:
-            robo_x = robo_x + move_x
-            robo_y = robo_y + move_y
-            cell = "{0},{1}".format(robo_x,robo_y)
-        house_locations.append(cell)
-
-print house_locations
-print len(set(house_locations))
-
+print houses_with_presents()
+print houses_with_presents(santas = 2)
