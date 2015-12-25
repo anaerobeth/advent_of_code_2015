2c54873cc00b5eda50053b2cf1ae08c5924cafb5
Beth Tenorio <betsky98@gmail.com>
2015-12-24 19:21:05 -0500

    solution for day8 - complete

--- day8.py
+++ day8.py
@@ -7,14 +7,25 @@ def chars_in_memory(l):
     return len(l) - 2
 
 
+def chars_of_encoded_string(l):
+    num_of_quote_chars = l.count('"')
+    num_of_chars = len(repr(l)) + num_of_quote_chars
+    return num_of_chars
+
+
 with open('day8_input.txt', 'r') as f:
-    total_chars_of_string = 0
+    total_chars_of_strings = 0
     total_chars_in_memory = 0
+    total_chars_of_encoded_strings = 0
 
     for line in f:
         l = line.rstrip()
-        total_chars_of_string += chars_of_string(l)
+        total_chars_of_strings += chars_of_string(l)
         total_chars_in_memory += chars_in_memory(l)
+        total_chars_of_encoded_strings += chars_of_encoded_string(l)
 
-    print total_chars_of_string - total_chars_in_memory
+    print 'Number of characters for string literals minus the number of characters in memory:'
+    print total_chars_of_strings - total_chars_in_memory
 
+    print 'Number of characters of encoded string minus the number of characters for string literals:'
+    print total_chars_of_encoded_strings - total_chars_of_strings
